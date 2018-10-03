2516c934806c3e558c789e43f65f17b905ab9da2
favour-umoru <43656344+favour-umoru@users.noreply.github.com>
2018-10-02 20:54:09 -0500

    Create mountainpassinitial.cpp

---
+++ mountainpassinitial.cpp
@@ -0,0 +1,11 @@
+#Include <iostream>
+#Include <math.h>
+
+void main()
+{
+  double mapRow, mapColumns;
+  
+  // Ask the user for the input
+  cout << "Enter the number of rows in the map" << endl;
+  cin >> mapRow
+}
