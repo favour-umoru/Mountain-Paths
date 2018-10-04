48685d4f3b3d215ff691960dab9babc6506a5ef6
favour-umoru <43656344+favour-umoru@users.noreply.github.com>
2018-10-04 10:52:17 -0500

    Update mountainpassinitial.cpp

--- mountainpassinitial.cpp
+++ mountainpassinitial.cpp
@@ -3,9 +3,13 @@
 
 void main()
 {
+  // Declare the variables
   double mapRow, mapColumns;
+  vector <int> vectorMap;
   
   // Ask the user for the input
   cout << "Enter the number of rows in the map" << endl;
-  cin >> mapRow
+  cin >> mapRow;
+  cout << "Enter the number of col in the map" << endl;
+  cin >> mapColumns;
 }
