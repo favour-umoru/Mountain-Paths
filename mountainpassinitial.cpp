b2bababce825696e6da3addd07c6dcbac32726a9
favour-umoru <43656344+favour-umoru@users.noreply.github.com>
2018-10-04 18:35:45 -0500

    Update mountainpassinitial.cpp

--- mountainpassinitial.cpp
+++ mountainpassinitial.cpp
@@ -4,12 +4,29 @@
 using namespace std;
 
 // Create functions which will be used in file
+double minElevation()
+{
+    
+}
 
+double minElevation()
+{
+  
+}
+
+void computeColor(int )
+{
+  int shadeGray = 0;
+  
+  shadeGray = (elevationP
+}
 
 void main()
 {
   // Declare the variables
-  double mapRow, mapColumns;
+  ifstream readFile;
+  int mapRow, mapColumns;
+  
   vector <vector<int>> vectorMap;
   string filename; 
   
@@ -21,15 +38,33 @@ void main()
   cout << "Enter the filename" << endl;
   cin >> filename;
   
-   if (!vectorMap.is_open()) 
+  // Try to open file
+   readFile.open(filename);
+  
+  // If the file does not open
+   if (!readFile.is_open()) 
    {
-      cout << "Could not open file " << filename << endl;
+      cout << "Error: Could not open file " << filename << endl;
       return 1;
    }
   
-  // Read from a file into a vector
-  vectorMap >> 
+  if (cin.fail()) 
+   {
+      cout << "Error " << filename << endl;
+      return 1;
+   }
+  
+  // Read from a file into a vector using a while loop
+  while (!readFile.eof()) 
+  {
+    // Read from file into a nested loop
+      for(int i = 0; i < mapRow; i++){
+          for (int j = 0; j < mapColumns; j++){
+              filename  >> vectorMap[i][j];
+          }
+      }
+  }
   
   // Done with file, close it
-   inFS.close();
+   readFile.close();
 }
