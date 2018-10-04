bc2158509f712d137f34eab34b26caba8b502098
favour-umoru <43656344+favour-umoru@users.noreply.github.com>
2018-10-04 18:13:51 -0500

    Update mountainpassinitial.cpp

--- mountainpassinitial.cpp
+++ mountainpassinitial.cpp
@@ -1,15 +1,35 @@
-#Include <iostream>
-#Include <math.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+// Create functions which will be used in file
+
 
 void main()
 {
   // Declare the variables
   double mapRow, mapColumns;
-  vector <int> vectorMap;
+  vector <vector<int>> vectorMap;
+  string filename; 
   
   // Ask the user for the input
   cout << "Enter the number of rows in the map" << endl;
   cin >> mapRow;
   cout << "Enter the number of col in the map" << endl;
   cin >> mapColumns;
+  cout << "Enter the filename" << endl;
+  cin >> filename;
+  
+   if (!vectorMap.is_open()) 
+   {
+      cout << "Could not open file " << filename << endl;
+      return 1;
+   }
+  
+  // Read from a file into a vector
+  vectorMap >> 
+  
+  // Done with file, close it
+   inFS.close();
 }
